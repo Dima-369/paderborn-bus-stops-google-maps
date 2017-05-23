2da745e0e2786f0a7e170690e59596e1bc87221f
Dmytro Butemann <dmytro.butemann@gmail.com>
2017-05-23 09:47:32 +0200

    Add source code files and cleanup README

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# Displays all bus stops in Padernborn on Google Maps with markers
+# Displays all bus stops in Padernborn with Google Maps markers
+
+On loading the website, it queries the times from all bus stops which takes quite some time.
 
 You have to refresh the site for the times to update.
-
-On loading the website, it also queries the times from all bus stops which takes quite some time.
 
 ## Notes
 
 * The `bus_stops.json` were scraped beforehand, a very long time ago, but I lost the script and don't quite remember where from
   * The latitude and longitude are not perfectly accurate
+* You can set `showBusTimes` in `maps.js` to false, to only display the markers with the bus stop name which loads extremely fast
+* The code is very old, and not in its best state
 
 ## Screenshots
 
---
+++ maps.js
@@ -0,0 +1,110 @@
+var map;
+var directionsDisplay;
+// if false, only the markers are displayed without any downloaded bus information which is almost
+// instant compared to downloading the bus data for each stop
+var showBusTimes = false;
+
+function initialize() {
+
+    directionsDisplay = new google.maps.DirectionsRenderer();
+
+    var mapOptions = {
+        zoom: 15,
+        // centering on the University of Paderborn
+        center: new google.maps.LatLng(51.706768, 8.771104),
+        clickableIcons: false
+    };
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
+
+    $.getJSON('bus_stops.json').done(
+        function (data) {
+            $.each(data, function (key_first, value_first) {
+
+                var infoWindowContent = "";
+
+                if (showBusTimes) {
+                    var yql = 'http://query.yahooapis.com/v1/public/yql?'
+                        + 'q=' + encodeURIComponent('select * from json where url=@url')
+                        + '&url=' + encodeURIComponent("http://padersprinter.de/internetservice/services/passageInfo/stopPassages/stop?stop=" + key_first + "&mode=arrival")
+                        + '&format=json&callback=?';
+
+                    $.getJSON(yql, test_func);
+
+                    function test_func(json) {
+
+                        if (json.query.count) {
+
+                            var data = json.query.results.json;
+                            var hasContent = false;
+                            infoWindowContent = "<h3 style=\"text-align: center\">" + value_first.name + "</h3><ul>";
+
+                            if (data.actual) {
+                                $.each(data.actual, function (key, val) {
+
+                                    if (typeof val.patternText != 'undefined' && typeof val.direction != 'undefined' && typeof val.actualTime != 'undefined') {
+                                        infoWindowContent += "<li>" + val.patternText + " (" + val.direction + "): " + "<b>" + val.actualTime + "</b></li>";
+                                        hasContent = true;
+                                    }
+                                });
+                            }
+
+                            if (hasContent) {
+                                infoWindowContent += "</ul>";
+                                var infoWindow = new google.maps.InfoWindow({
+                                    content: infoWindowContent
+                                });
+
+                                var marker = new google.maps.Marker({
+                                    position: new google.maps.LatLng(value_first.latitude, value_first.longitude),
+                                    map: map
+                                });
+
+                                marker.addListener('click', function () {
+                                    if (!marker.open) {
+                                        infoWindow.open(map, marker);
+                                        marker.open = true;
+                                    } else {
+                                        infoWindow.close();
+                                        marker.open = false;
+                                    }
+                                });
+                            }
+
+                        } else {
+                            return false;
+                        }
+                    }
+
+                } else {
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(value_first.latitude, value_first.longitude),
+                        map: map
+                    });
+                    var infoWindow = new google.maps.InfoWindow({
+                        content: "<h3>" + value_first.name + "</h3>"
+                    });
+
+                    marker.addListener('click', function () {
+                        if (!marker.open) {
+                            infoWindow.open(map, marker);
+                            marker.open = true;
+                        } else {
+                            infoWindow.close();
+                            marker.open = false;
+                        }
+                    });
+                }
+            });
+        }
+    )
+}
+
+/**
+ * Called by the Google Maps callback when it's done loading.
+ */
+function initMap() {
+    var directionsService = new google.maps.DirectionsService();
+    initialize()
+}
